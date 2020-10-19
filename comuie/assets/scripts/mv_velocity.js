f626691c42b75c56b317316ea602e4758c7965b3
totikuma <43670695+totikuma@users.noreply.github.com>
2020-10-19 11:39:05 +0900

    ver.03

---
+++ comuie/assets/scripts/mv_velocity.js
@@ -0,0 +1,13 @@
+$(function () {
+        
+                $('.p-mv__kumo01').velocity({
+                        marginLeft: 1400,
+                    },
+                    {
+                    duration: 5000,
+                    easing: 'swing',
+                    loop:100,
+                })
+        
+
+});
