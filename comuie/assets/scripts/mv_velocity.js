fa25cdbfd7cc65cced88b0a69486338cc2bf62a3
totikuma <43670695+totikuma@users.noreply.github.com>
2020-10-19 14:11:24 +0900

    ver.04

--- comuie/assets/scripts/mv_velocity.js
+++ comuie/assets/scripts/mv_velocity.js
@@ -1,13 +1,24 @@
 $(function () {
-        
-                $('.p-mv__kumo01').velocity({
-                        marginLeft: 1400,
-                    },
-                    {
-                    duration: 5000,
-                    easing: 'swing',
-                    loop:100,
-                })
-        
+
+    let km_a = function () {
+        $('.p-mv__kumo01').velocity({
+            marginLeft: 1400,
+        }, {
+            duration: 45000,
+
+        })
+    }
+
+    km_a();
+    let km_b = function () {
+        $('.p-mv__kumo02').velocity({
+            marginLeft: 1400,
+        }, {
+            duration: 50000,
+
+        })
+    }
+
+    km_b();
 
 });
