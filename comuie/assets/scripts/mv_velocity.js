db4f48e89ba2230beb074ea7f6a05de8a5bf27e1
shunki-jp <49384507+shunki-jp@users.noreply.github.com>
2020-10-20 17:08:38 +0900

    ver6

---
+++ comuie/assets/scripts/mv_velocity.js
@@ -0,0 +1,24 @@
+$(function () {
+
+    let km_a = function () {
+        $('.p-mv__kumo01').velocity({
+            marginLeft: 1400,
+        }, {
+            duration: 65000,
+
+        })
+    }
+
+    km_a();
+    let km_b = function () {
+        $('.p-mv__kumo02').velocity({
+            marginLeft: 1400,
+        }, {
+            duration: 70000,
+
+        })
+    }
+
+    km_b();
+
+});
